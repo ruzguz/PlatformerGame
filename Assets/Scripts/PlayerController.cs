db22b0aef2fb184a584c94dbfc7572909964733e
Rusben Guzman <rusbenguzman@gmail.com>
2020-05-02 20:58:31 -0400

    Player Movement with UI Arrows

---
+++ Assets/Scripts/PlayerController.cs
@@ -0,0 +1,41 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class PlayerController : MonoBehaviour
+{
+
+    // Components vars
+    Rigidbody2D _playerRB;
+
+    // General Vars
+    [SerializeField]
+    float _speed = 10f;
+
+
+    // Is called when the script instance is loaded
+    void Awake() 
+    {
+        _playerRB = GetComponent<Rigidbody2D>();
+    }
+
+    // Start is called before the first frame update
+    void Start()
+    {
+        
+    }
+
+    // Update is called once per frame
+    void Update()
+    {
+
+    }
+
+    public void Run(float axis) 
+    {
+        // Change player velocity 
+        _playerRB.velocity = new Vector2(axis * _speed, _playerRB.velocity.y);
+    }
+
+
+}
---
+++ Assets/Scripts/RunButtonController.cs
@@ -0,0 +1,54 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using UnityEngine.EventSystems;
+
+public class RunButtonController : MonoBehaviour
+{
+
+    // Consts
+    public const float CHANGE_RATE = 0.1f;
+
+    // General vars 
+    bool _isPressed = false;
+    float _axis = 0;
+    [SerializeField] 
+    int _direction;
+
+    // Other GameObject references 
+    public PlayerController player;
+
+
+    // Update is called every frame, if the MonoBehaviour is enabled.
+    void Update()
+    {
+        // Calculating player speed and direction
+        if (_isPressed) 
+        {
+            _axis = _axis = Mathf.Lerp(_axis, 1, CHANGE_RATE);
+            // Move player
+            player.Run(_axis * _direction);
+        }
+
+        Debug.Log(_axis);
+    }
+
+
+    // Action when the player press the run button
+    public void Press()
+    {
+
+        _isPressed = true;
+    }
+
+    // Action when the player stop pressing the run button 
+    public void StopPressing() 
+    {
+        _isPressed = false;  
+        _axis = 0; 
+        player.Run(0);
+    }
+
+
+}
+
