3a9d0eab8c482536a82b34b7df09a3f26815342e
Rusben Guzman <rusbenguzman@gmail.com>
2020-05-04 09:43:37 -0400

    Running button bugs fixed, when you press one of them the other one is disabled

--- Assets/Scripts/RunButtonController.cs
+++ Assets/Scripts/RunButtonController.cs
@@ -2,6 +2,7 @@
 using System.Collections.Generic;
 using UnityEngine;
 using UnityEngine.EventSystems;
+using UnityEngine.UI;
 
 public class RunButtonController : MonoBehaviour
 {
@@ -17,7 +18,15 @@ public class RunButtonController : MonoBehaviour
 
     // Other GameObject references 
     public PlayerController player;
+    public Button otherButton;
 
+    // Start is called on the frame when a script is enabled just before
+    // any of the Update methods is called the first time.
+    void Start()
+    {
+        // Getting the other arrow to disable it when this is pressed
+        otherButton = GameObject.Find(this.name == "RightArrow"?"LeftArrow":"RightArrow").GetComponent<Button>();
+    }
 
     // Update is called every frame, if the MonoBehaviour is enabled.
     void Update()
@@ -25,7 +34,7 @@ public class RunButtonController : MonoBehaviour
         // Calculating player speed and direction
         if (_isPressed) 
         {
-            _axis = _axis = Mathf.Lerp(_axis, 1, CHANGE_RATE);
+            _axis = Mathf.Lerp(_axis, 1, CHANGE_RATE);
             // Move player
             player.Run(_axis * _direction);
         }
@@ -35,16 +44,23 @@ public class RunButtonController : MonoBehaviour
     // Action when the player press the run button
     public void Press()
     {
-
+        // if button is disble don't do any action
+        if (!GetComponent<Button>().interactable) return;
+        // set buttton as pressed and disable the other arrow 
         _isPressed = true;
+        otherButton.interactable = false;
     }
 
     // Action when the player stop pressing the run button 
     public void StopPressing() 
     {
+        // if button is disble don't do any action
+        if (!GetComponent<Button>().interactable) return;
+        // Stop the player and enable the other arrow
         _isPressed = false;  
         _axis = 0; 
         player.Run(0);
+        otherButton.interactable = true;
     }
 
 
